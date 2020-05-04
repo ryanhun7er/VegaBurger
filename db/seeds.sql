b0c18add03f06e0a21627c18f21c692659972ee5
Ryan Hunter <ryanhwilkinson@icloud.com>
2020-05-04 11:19:14 -0500

    add schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured ) VALUES ('Beyond Burger with Vegan Mozarella', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Supreme Impossible Burger with tomato', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan Beyond Sausage Burger, no cheese', true);
