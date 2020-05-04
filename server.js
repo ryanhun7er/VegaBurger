abbf7f94ae0f0f817b0e48c5cb5422823f05168f
Ryan Hunter <ryanhwilkinson@icloud.com>
2020-05-04 11:25:04 -0500

    update server.js

--- server.js
+++ server.js
@@ -16,3 +16,13 @@ var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgerController.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
